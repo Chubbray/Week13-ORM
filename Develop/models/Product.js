d5c172d1cb62ed3c17a5f6a32f6eeae439834b96
Chubbray <chubbray1@gmail.com>
2020-08-23 19:16:45 -0600

    got everything to work

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -5,7 +5,7 @@ const sequelize = require('../config/connection');
 const { validate } = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -14,24 +14,27 @@ Product.init(
 
     id: {
       type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
 
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      isDecimal: true,
+      vanidate: {
+        isDecimal: true,
+      }
     },
 
     stock: {
       type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: {
-          max: [10]
-        },
+      allowNull: false,
+      validate: {
+        max: 10,
         isNumeric: true,
+      },
+
     },
 
     category_id: {
